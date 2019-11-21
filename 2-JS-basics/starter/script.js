cff337f7e9b3dfbf988d2473c927ff83b9861e5c
prorigene <morigene52@outlook.com>
2019-11-21 13:38:42 +0200

    Starting code and learn data types

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,18 @@
+console.log('Hello world !!!');
+
+var firstName = "John";
+ console.log(firstName);
+
+ var lastName = 'Smith';
+ var age = 28;
+
+ // different data types
+ var fullAge = true;
+ console.log(fullAge);
+
+
+ var job;
+ console.log(job)
+
+ job = 'Teacher'
+ console.log(job);

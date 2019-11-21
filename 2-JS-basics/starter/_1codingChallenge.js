7c3e55b2fd5cf780ec2e1187909297e6e732c748
prorigene <morigene52@outlook.com>
2019-11-21 20:13:21 +0200

    Operators, assignment and challenges

---
+++ 2-JS-basics/starter/_1codingChallenge.js
@@ -0,0 +1,12 @@
+prompt( "What is the height of John and mass");
+var johnMass  = 95
+var johnHeight = 1.7
+prompt( "what is the heigh and mass of mark ");
+var markMass = 80;
+var markHeigh = 1.5;
+
+var johnBMI = johnMass / (johnHeight * johnHeight);
+var markBMI = markMass / ( markMass * markMass);
+
+var isHigher = johnBMI > markBMI;
+console.log( 'Is Mark\'s BMI higher than John\'s BMI? '+isHigher)
--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -33,6 +33,7 @@ var firstName = "John";
  * Variables  mutation and type coercion
  */
 
+ 
 var firstName = 'John';
 var age = 28;
 //type coercion
@@ -46,7 +47,57 @@ console.log(firstName + ' is a '+ age + ' year old ' +job +' .Is he married? '+i
 // Variable mutation
 age = 'twenty eight';
 job = 'driver';
-alert(firstName + 'is a '+ age + ' year old '+ ' job '+ job + ' .Is he married? '+ isMarried);
+alert(firstName + ' is a '+ age + ' year old '+ ' job '+ job + ' .Is he married? '+ isMarried);
 
 var lastName = prompt( ' What is his last Name? ');
 console.log(firstName + ' '+lastName);
+
+/**************************
+ * BAsic operators 
+ */
+var year, yearJohn, yearMark;
+
+// Math operators
+now = 2018;
+yearJohn = now - 28 ;
+yearMark = now - 33;
+
+console.log(yearJohn);
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x ;
+console.log(typeof x);
+
+/*******************************************
+ * Operator precedence
+ */
+var now = 2018;
+var yeahJohn = 1989;
+var fullAge  = 18;
+
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge; // true
+console.log(isFullAge);
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+//Multiple assignment
+var x, y;
+x = (3 + 5)* 4 - 6;  // 8 * 4 -6 // 32 - 6
+
+// More operators 
+x  = x + 2  // assignment,condition an exponential is from right to left

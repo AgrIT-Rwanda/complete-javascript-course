6c4a61bc1e2c2a8f52c61296b88cf1afbd53cdc7
prorigene <morigene52@outlook.com>
2019-11-25 14:41:02 +0200

    3rd challenges on function and arrays

---
+++ 2-JS-basics/starter/_3codingChallenge.js
@@ -0,0 +1,26 @@
+
+function tipCalculator(bill){
+
+    if( bill > 200){
+     totalTipPay = bill * 0.1 ;
+    }else if( bill > 50 && bill <= 200){
+        totalTipPay = bill * 0.15 ;
+    }else{
+        totalTipPay = bill * 0.2 ;
+    }
+
+     return totalTipPay;
+}
+
+bills = [124, 48, 268];
+var tips = [tipCalculator(bills[0]), tipCalculator(bills[1]), 
+tipCalculator(bills[2])];
+var finalValues = [
+    bills[0] + tips[0],
+    bills[1] + tips[1],
+    bills[2] + tips[2]
+];
+console.log(tips);
+console.log(finalValues);
+
+

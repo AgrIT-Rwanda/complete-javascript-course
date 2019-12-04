c594ebecdf564cb8ecc74b7724413f0de15373ea
prorigene <morigene52@outlook.com>
2019-12-04 17:16:23 +0200

    First class: function returning, generic function

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -101,3 +101,28 @@ var ages = arrayCalc(years, calculateAge);
 var fullAges =  arrayCalc(ages, isFullAge);
 console.log(ages);
 console.log(fullAges);
+
+// Lecture: Function returning functions.
+
+function interviewQuestion(job){
+
+    if(job === 'designer'){
+
+        return function(name){
+
+            console.log(name + ', can you please explain what UX design is? ');
+        }
+    }else if( job === 'teacher'){
+        return function(name){
+            console.log('What subject do you teach, ' +name + '?');
+        }
+    }else{
+
+        return function(name){
+            console.log('Hello' +name+ ', What do you do ?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');   // u can write generic function and call of batch of function in it
+teacherQuestion('john');    //
